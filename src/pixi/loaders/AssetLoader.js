drkibitz/pixi.js

Browserify refactor
- Pixi is now completely CommonJS
- Works when compiled through browserify
- Can work in node with node-canvas with a little work
- All examples still work as before with no modifications to them
- Still generates complete compiled library, with PIXI global (building and testing is still done for it)
- Cleanup of course happened in the course of this refactor
- Removed all circular dependencies, but still needs better organization
- Some file renaming occurred to adhere to capitalized constructor module and lowercase non-constructor module

src/pixi/loaders/AssetLoader.js
@@ -1,11 +1,22 @@
 /**
  * @author Mat Groves http://matgroves.com/ @Doormat23
  */
+'use strict';
+
+var EventTarget = require('../events/EventTarget');
+
+/**
+ * Maps file extension to loader types
+ *
+ * @property loadersByType
+ * @type Object
+ */
+var loadersByType = {};
 
 /**
  * A Class that loads a bunch of images / sprite sheet / bitmap font files. Once the
- * assets have been loaded they are added to the PIXI Texture cache and can be accessed
- * easily through PIXI.Texture.fromImage() and PIXI.Sprite.fromImage()
+ * assets have been loaded they are added to the Texture cache and can be accessed
+ * easily through Texture.fromImage() and Sprite.fromImage()
  * When all items have been loaded this class will dispatch a "onLoaded" event
  * As each individual item is loaded this class will dispatch a "onProgress" event
  *
@@ -18,9 +29,9 @@
  *      data formats include "xml" and "fnt".
  * @param crossorigin {Boolean} Whether requests should be treated as crossorigin
  */
-PIXI.AssetLoader = function(assetURLs, crossorigin)
+function AssetLoader(assetURLs, crossorigin)
 {
-    PIXI.EventTarget.call(this);
+    EventTarget.call(this);
 
     /**
      * The array of asset URLs that are going to be loaded
@@ -37,26 +48,9 @@ PIXI.AssetLoader = function(assetURLs, crossorigin)
      * @type Boolean
      */
     this.crossorigin = crossorigin;
+}
 
-    /**
-     * Maps file extension to loader types
-     *
-     * @property loadersByType
-     * @type Object
-     */
-    this.loadersByType = {
-        "jpg":  PIXI.ImageLoader,
-        "jpeg": PIXI.ImageLoader,
-        "png":  PIXI.ImageLoader,
-        "gif":  PIXI.ImageLoader,
-        "json": PIXI.JsonLoader,
-        "anim": PIXI.SpineLoader,
-        "xml":  PIXI.BitmapFontLoader,
-        "fnt":  PIXI.BitmapFontLoader
-    };
-
-
-};
+var proto = AssetLoader.prototype;
 
 /**
  * Fired when an item has loaded
@@ -68,15 +62,12 @@ PIXI.AssetLoader = function(assetURLs, crossorigin)
  * @event onComplete
  */
 
-// constructor
-PIXI.AssetLoader.prototype.constructor = PIXI.AssetLoader;
-
 /**
  * Starts loading the assets sequentially
  *
  * @method load
  */
-PIXI.AssetLoader.prototype.load = function()
+proto.load = function load()
 {
     var scope = this;
 
@@ -91,7 +82,7 @@ PIXI.AssetLoader.prototype.load = function()
         var fileName = this.assetURLs[i];
         var fileType = fileName.split(".").pop().toLowerCase();
 
-        var loaderClass = this.loadersByType[fileType];
+        var loaderClass = loadersByType[fileType];
         if(!loaderClass)
             throw new Error(fileType + " is an unsupported file type");
 
@@ -108,7 +99,7 @@ PIXI.AssetLoader.prototype.load = function()
  * @method onAssetLoaded
  * @private
  */
-PIXI.AssetLoader.prototype.onAssetLoaded = function()
+proto.onAssetLoaded = function onAssetLoaded()
 {
     this.loadCount--;
     this.dispatchEvent({type: "onProgress", content: this});
@@ -121,3 +112,9 @@ PIXI.AssetLoader.prototype.onAssetLoaded = function()
     }
 };
 
+AssetLoader.registerLoaderType = function registerLoaderType(type, constructor)
+{
+    loadersByType[type] = constructor;
+};
+
+module.exports = AssetLoader;

drkibitz/node-pixi

Conflicts:
	src/pixi/InteractionManager.js
	src/pixi/extras/Spine.js
	src/pixi/loaders/AssetLoader.js
	src/pixi/utils/Utils.js

src/pixi/loaders/AssetLoader.js
@@ -1,11 +1,22 @@
 /**
  * @author Mat Groves http://matgroves.com/ @Doormat23
  */
+'use strict';
+
+var EventTarget = require('../events/EventTarget');
+
+/**
+ * Maps file extension to loader types
+ *
+ * @property loadersByType
+ * @type Object
+ */
+var loadersByType = {};
 
 /**
  * A Class that loads a bunch of images / sprite sheet / bitmap font files. Once the
- * assets have been loaded they are added to the PIXI Texture cache and can be accessed
- * easily through PIXI.Texture.fromImage() and PIXI.Sprite.fromImage()
+ * assets have been loaded they are added to the Texture cache and can be accessed
+ * easily through Texture.fromImage() and Sprite.fromImage()
  * When all items have been loaded this class will dispatch a "onLoaded" event
  * As each individual item is loaded this class will dispatch a "onProgress" event
  *
@@ -18,9 +29,9 @@
  *      data formats include "xml" and "fnt".
  * @param crossorigin {Boolean} Whether requests should be treated as crossorigin
  */
-PIXI.AssetLoader = function(assetURLs, crossorigin)
+function AssetLoader(assetURLs, crossorigin)
 {
-    PIXI.EventTarget.call(this);
+    EventTarget.call(this);
 
     /**
      * The array of asset URLs that are going to be loaded
@@ -37,26 +48,9 @@ PIXI.AssetLoader = function(assetURLs, crossorigin)
      * @type Boolean
      */
     this.crossorigin = crossorigin;
+}
 
-    /**
-     * Maps file extension to loader types
-     *
-     * @property loadersByType
-     * @type Object
-     */
-    this.loadersByType = {
-        "jpg":  PIXI.ImageLoader,
-        "jpeg": PIXI.ImageLoader,
-        "png":  PIXI.ImageLoader,
-        "gif":  PIXI.ImageLoader,
-        "json": PIXI.JsonLoader,
-        "anim": PIXI.SpineLoader,
-        "xml":  PIXI.BitmapFontLoader,
-        "fnt":  PIXI.BitmapFontLoader
-    };
-
-
-};
+var proto = AssetLoader.prototype;
 
 /**
  * Fired when an item has loaded
@@ -68,15 +62,12 @@ PIXI.AssetLoader = function(assetURLs, crossorigin)
  * @event onComplete
  */
 
-// constructor
-PIXI.AssetLoader.prototype.constructor = PIXI.AssetLoader;
-
 /**
  * Starts loading the assets sequentially
  *
  * @method load
  */
-PIXI.AssetLoader.prototype.load = function()
+proto.load = function load()
 {
     var scope = this;
 
@@ -91,7 +82,7 @@ PIXI.AssetLoader.prototype.load = function()
         var fileName = this.assetURLs[i];
         var fileType = fileName.split(".").pop().toLowerCase();
 
-        var Constructor = this.loadersByType[fileType];
+        var Constructor = loadersByType[fileType];
         if(!Constructor)
             throw new Error(fileType + " is an unsupported file type");
 
@@ -108,7 +99,7 @@ PIXI.AssetLoader.prototype.load = function()
  * @method onAssetLoaded
  * @private
  */
-PIXI.AssetLoader.prototype.onAssetLoaded = function()
+proto.onAssetLoaded = function onAssetLoaded()
 {
     this.loadCount--;
     this.dispatchEvent({type: "onProgress", content: this});
@@ -121,3 +112,9 @@ PIXI.AssetLoader.prototype.onAssetLoaded = function()
     }
 };
 
+AssetLoader.registerLoaderType = function registerLoaderType(type, constructor)
+{
+    loadersByType[type] = constructor;
+};
+
+module.exports = AssetLoader;